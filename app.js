e8f668d92fc565c2f8b404d411190e7bfe353168
DDKUK <daniel@pixelinspiration.co.uk>
2015-04-22 17:22:03 +0100

    added a few test pages using the jade format.
    updated the app.js file to add a locals object to the 'response object'
    page and viewcount is increased.

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
+
 //load our node_modules
 var express = require("express"),
-	path = require("path");
-
+	path = require("path"),
+	fs = require("fs");
 //create our app instance
 var app = express();
-
 //configure our jade
+var dirViews = path.join( __dirname, "views" );
 app.set("view engine", "jade");
-app.set("views", path.join( __dirname, "views" ) );
-
+app.set("views", dirViews );
 //create a route to respond to requests
-app.get("/", function( req, res, next ){
-	res.render("index");
-});
+app.get("/", renderHtml );
+app.get("/:page", renderHtml );
+
+var viewCount = 0;
+
+function renderHtml( req, res, next ){
+	var page = req.params.page || "index";
+	var pathPage = path.join( dirViews, page + ".jade" );
+	//check the required page exists
+	fs.exists( pathPage, function( boolExists ){
+		if( boolExists ){
+			res.locals = {
+				path : req.path,
+				page : page,
+				dirRoot : __dirname,
+				viewCount : ++viewCount
+			};
+			
+			res.render( page );
+		}else{
+			next();
+		}
+	} );
+}
 
 //expose public directory
 var dirPublic = path.join( __dirname, "public" );
 app.use( express.static( dirPublic ) );
-
+//our 404
+app.use( function( req, res, next ){
+	res.render("404");
+} );
 //listen on a port
 app.listen( 4000 );
