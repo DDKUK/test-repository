af615c791f3c69ca52df1d3933d2221a0faf67f9
DDKUK <daniel@pixelinspiration.co.uk>
2015-04-22 16:31:07 +0100

    Installed JADE rendering engine to the site
    webpages are now moved to the view folder and are handled by jade.
    
    Once added, Jade should be installed using npm

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 //load our node_modules
 var express = require("express"),
 	path = require("path");
+
 //create our app instance
 var app = express();
+
+//configure our jade
+app.set("view engine", "jade");
+app.set("views", path.join( __dirname, "views" ) );
+
 //create a route to respond to requests
+app.get("/", function( req, res, next ){
+	res.render("index");
+});
+
+//expose public directory
 var dirPublic = path.join( __dirname, "public" );
 app.use( express.static( dirPublic ) );
+
 //listen on a port
 app.listen( 4000 );
