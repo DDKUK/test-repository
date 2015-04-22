dd25ff88c11285f763c5bc637a3e7d1ae93a5ab3
DDKUK <daniel@pixelinspiration.co.uk>
2015-04-22 15:11:10 +0100

    Updated Main App to innitialise as listener, updated package details JSON object

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+//load our node_modules
+var express = require("express");
+//create our app instance
+var app = express();
+//create a route to respond to requests
+app.get("/", function( req, res, next ){
+	console.log("Path", req.path);
+	console.log("Headers", req.headers);
+	res.send("Hello World");
+});
+//listen on a port
+app.listen( 4000 );
